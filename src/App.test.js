e5f780541cdace7713472f0b6a626747dca58837
abhaysampath <abhaysampath@gmail.com>
2024-05-11 21:57:09 -0400

    Added test placeholders for cicd

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders without crashing', () => {
+    render(<App />);
+    // TODO: assertions to test rendering of App component
+  });
+
+  // TODO: add more tests
 });
